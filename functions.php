dfea0683f0f6deed6b3a0a28ebfb66aac56ba365
Mamun Morshed <mamun@audacityit.com>
2016-11-18 01:56:02 +0600

    Added menu. Primary menu and footer menu has been registered and template code for the primary menu has been placed.

--- functions.php
+++ functions.php
@@ -1,5 +1,17 @@
 <?php 
 
+if (!function_exists('treehouse_portfolio_setup')) {
+	function treehouse_portfolio_setup(){
+		add_theme_support( 'menus' );
+		register_nav_menus( array( 
+			'primary-menu' => __( 'Primary Menu'),
+			'footer-menu' => __( 'Footer Menu')
+		 ) );
+	}
+}
+add_action( 'after_setup_theme', 'treehouse_portfolio_setup' );
+
+
 function thp_mm_enqueue_css(){
 	wp_enqueue_style( 'normalize_css', get_stylesheet_directory_uri().'/assets/css/normalize.css');
 	wp_enqueue_style( 'foundation_css', get_stylesheet_directory_uri().'/assets/css/foundation.css');
